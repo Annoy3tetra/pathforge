a5a4aeb7614786d584744b1aa9cbbfa07a68203f
Annoy3tetra <harsh185546@gmail.com>
2026-05-08 17:39:40 +0530

    Implemented JWT authentication

---
+++ backend/app/api/auth.py
@@ -0,0 +1,90 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.models.user import User
+from app.schemas.user import (
+    UserRegister,
+    UserLogin,
+    TokenResponse
+)
+from app.core.security import (
+    hash_password,
+    verify_password
+)
+from app.core.jwt_handler import create_access_token
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+@router.post("/register")
+def register_user(
+    user: UserRegister,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User registered successfully"
+    }
+
+
+@router.post(
+    "/login",
+    response_model=TokenResponse
+)
+def login_user(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if not existing_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    valid_password = verify_password(
+        user.password,
+        existing_user.password
+    )
+
+    if not valid_password:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    access_token = create_access_token(
+        data={
+            "sub": existing_user.email
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.models.user import User
 
+from app.api.auth import router as auth_router
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="PathForge API")
+
+app.include_router(auth_router)
 
 
 @app.get("/")
