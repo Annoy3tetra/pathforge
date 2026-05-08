4cb7e6bc897fcce8dec0331ecdc82bfc35716426
Annoy3tetra <harsh185546@gmail.com>
2026-05-08 17:31:48 +0530

    Setup PostgreSQL and SQlAlchemy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.db.database import engine, Base
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="PathForge API")
+
 
 @app.get("/")
 def home():
